f8b5679bf449df71e20d4eaa442852c1e3914285
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-04-17 18:49:56 -0300

    TypeScript for Angular

---
+++ TypeScriptParaAngular/src/index.ts
@@ -0,0 +1,15 @@
+type heroi = {
+    name: string;
+    vulgo: string;
+}
+
+function printaObjeto(pessoa: heroi) {
+    console.log(pessoa);
+    
+}
+printaObjeto({
+    name: 'Bruce Waine',
+    vulgo: 'Batman'
+})
+
+console.log('');
