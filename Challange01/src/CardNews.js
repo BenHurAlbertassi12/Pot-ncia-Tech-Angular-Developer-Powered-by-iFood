fb88dc1a726d2d05a666460d3ba7b7e4a44a7e98
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-04-17 14:51:28 -0300

    ,

--- Challange01/src/CardNews.js
+++ Challange01/src/CardNews.js
@@ -2,14 +2,38 @@ class CardNews extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open" });
-        shadow.innerHTML = "<h1>Ola Benhur</h1>"
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
     }
     build() {
-     
+        const componentRoot = document.createElement('div')
+        componentRoot.setAttribute('class', 'card')
+
+        const cardLeft = document.createElement('div')
+        cardLeft.setAttribute('class', 'card_left')
+
+        const autor = document.createElement('span')     
+        const linkTitle = document.createElement('a')       
+        const paragrafo = document.createElement('p')
+
+        cardLeft.appendChild(autor)
+        cardLeft.appendChild(linkTitle)
+        cardLeft.appendChild(paragrafo)
+        
+        const cardRigt = document.createElement('div')
+        cardRigt.setAttribute('class', 'card_rigth')
+
+        const newImage = document.createElement('img')
+        cardRigt.appendChild(newImage)
+
+        componentRoot.appendChild(cardLeft)
+        componentRoot.appendChild(cardRigt)
+
+        return componentRoot
     }
 
     styles() {
-        
+
     }
 }
 customElements.define('card-news', CardNews)
