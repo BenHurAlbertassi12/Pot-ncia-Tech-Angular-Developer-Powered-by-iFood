805c923170fe8efd83f0c232e3a159f533ba12e7
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-04-28 13:45:06 -0300

    Diretivas

--- lifeCycleHooks/life-cycle/src/app/app.module.ts
+++ lifeCycleHooks/life-cycle/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { ChackSampleComponent } from './chack-sample/chack-sample.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     ChackSampleComponent,
-    CardComponent
+    CardComponent,
+    CompAtributosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- lifeCycleHooks/life-cycle/src/app/card/card.component.ts
+++ lifeCycleHooks/life-cycle/src/app/card/card.component.ts
@@ -15,7 +15,7 @@ export class CardComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
   adicionar() {
     this.produtos.push('BenHur')   
